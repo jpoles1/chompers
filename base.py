0894b6efe70bb1933ec7fee0bd649ab2c97bb268
jpoles1 <jpoles1@gmail.com>
2015-02-16 11:15:21 -0600

    Genome creation works!

--- base.py
+++ base.py
@@ -3,15 +3,20 @@ import random
 #Start by creating a population of organisms, each with two chromosomes encoding selection instructions.
 pop = 100
 worldsize=1000
+initmaxmove = 4
 pp = pprint.PrettyPrinter(indent=4)
 def createPop():
-	population = [chomper(1) for count in range(0, 100)]
+	population = [chomper(4) for count in range(0, 100)]
 	print population[5].genome
 class chomper():
 	id=0
 	def __init__(self, genomesize):
 		self.genomesize = genomesize
-		self.genome = random.choice(["u","d","l","r"])
+		genome = ""
+		for i in range(0, genomesize):
+			genome = genome+random.choice(["u","d","l","r"])
+			genome = genome+str(random.choice(range(1, initmaxmove)))
+		self.genome = genome
 		self.id = chomper.id
 		chomper.id+=1
 createPop()
